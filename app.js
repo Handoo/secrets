012404f1f309b7cf1f13acaf1b0fe7e9f2fda34f
Handoo <r.hans4486@gmail.com>
2019-05-20 21:56:29 -0700

    Final implementation of Passportjs

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const session = require('express-session');
 
 const app = express();
 
@@ -46,7 +46,6 @@ app.get('/', function(req, res){
 
 app.get('/secrets', function(req, res){
     if (req.isAuthenticated()){
-        console.log('secrets');
         res.render('secrets');
     } else {
         res.redirect('/login');
@@ -57,12 +56,24 @@ app.get('/secrets', function(req, res){
 app.route('/login')
 
 .get(function(req, res){
-    
+    res.render('login');    
 })
 
 .post(function(req, res){
-    ;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, function(err, user){
+                res.redirect('/secrets');
+            })
+        }
+    })
 });
 
 //When a user get and post on the Register page
@@ -73,7 +84,6 @@ app.route('/register')
 })
 
 .post(function(req, res){
-
     User.register({username: req.body.username}, req.body.password, function(err, user){
         if(err){
             console.log(err);
@@ -86,7 +96,12 @@ app.route('/register')
     });
 });
 
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
 const port = 3000;
 app.listen(port, function(){
     console.log('Server started on port ' + port);
-})
+});
